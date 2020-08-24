f16b44afbc661fa6ad5f2e2e50f5031021f11702
debajyatibanerjee0002 <debajyatibanerjee0002@gmail.com>
2020-08-24 12:46:16 +0530

    Update New Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-void main()=>runApp(new MaterialApp(
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(
-    primarySwatch: Colors.blueGrey,
-  ),
-  title: "DSC_TIB",
-));
+void main() => runApp(new MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      title: "DSC_TIB",
+    ));
 
 class MyApp extends StatefulWidget {
   @override
@@ -18,15 +18,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: new Text("DSC_TIB"),
-      ),
-      
-      url: "https://developer-student-club-tib.github.io/web/",
-      // withZoom: true,
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        WebviewScaffold(
+          url: "https://developer-student-club-tib.github.io/web/",
+          // withZoom: true,
+        ),
+      ],
     );
   }
 }
-
